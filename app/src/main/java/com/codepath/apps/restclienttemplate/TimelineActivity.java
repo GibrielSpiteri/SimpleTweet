3af44bb62e6c73c2b5b85828eca90c2cf9727f7e
Gibriel Spiteri <gibbyspiteri@gmail.com>
2019-03-10 16:50:57 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -0,0 +1,113 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+public class TimelineActivity extends AppCompatActivity {
+
+    private TwitterClient client;
+    private RecyclerView rvTweets;
+    private TweetAdapter adapter;
+    private List<Tweet> tweets;
+    private SwipeRefreshLayout swipeContainer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+
+        client = TwitterApp.getRestClient(this);
+        swipeContainer = findViewById(R.id.swipeContainer);
+        // Find the recycler view
+        rvTweets = findViewById(R.id.rvTweets);
+        // Initialize list of tweets and adapter from the data source
+        tweets = new ArrayList<>();
+        adapter = new TweetAdapter(this, tweets);
+        // Recycler view setup: layout manager and setting the adapter
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(adapter);
+
+        populateHomeTimeline();
+
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                populateHomeTimeline();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
+    }
+
+    private void populateHomeTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                Log.d("TwitterClient", response.toString());
+                // Iterate through the list of tweets
+                List<Tweet> tweetsToAdd = new ArrayList<>();
+                for(int i = 0; i < response.length(); i++){
+                    try{
+                        // Convert each JSON object into a tweet object
+                        JSONObject jsonTweetObj = response.getJSONObject(i);
+                        Tweet tweet = Tweet.fromJson(jsonTweetObj);
+
+                        // Add the tweet into our data source
+                        tweetsToAdd.add(tweet);
+                        Log.d("TwitterClient", tweet.user.profileImageUrl);
+                        // Notify adapter
+                        adapter.notifyItemInserted(tweets.size() - 1);
+                    } catch(JSONException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                // Clear existing data
+                adapter.clear();
+                // Add new tweets
+                adapter.addTweets(tweetsToAdd);
+                // Now we call setRefreshing(false) to signal refresh has finished
+                swipeContainer.setRefreshing(false);
+
+
+                //super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.e("TwitterClient", responseString);
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.e("TwitterClient", errorResponse.toString());
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+    }
+}
